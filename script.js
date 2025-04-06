c407d27e59fc573141346cbac822edcc0a62f87d
a-rya-shukla <2403031461013@paruluniversity.ac.in>
2025-04-06 15:29:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,184 +1,244 @@
-console.log('Lets write JavaScript');
-let currentSong = new Audio();
-let songs;
-let currFolder;
-
-function secondsToMinutesSeconds(seconds) {
-    if (isNaN(seconds) || seconds < 0) {
-        return "00:00";
-    }
-
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.floor(seconds % 60);
-
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-
-    return `${formattedMinutes}:${formattedSeconds}`;
-}
-
-async function getSongs(folder) {
-    currFolder = folder;
-    let a = await fetch(`/${folder}/`)
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a")
-    songs = []
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`/${folder}/`)[1])
-        }
-    }
-    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
-    songUL.innerHTML = ""
-    for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="img/music.svg" alt="">
-                            <div class="info">
-                                <div> ${song.replaceAll("%20", " ")}</div>
-                                <div>Harry</div>
-                            </div>
-                            <div class="playnow">
-                                <span>Play Now</span>
-                                <img class="invert" src="img/play.svg" alt="">
-                            </div> </li>`;
-    }
-
-    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
-        e.addEventListener("click", element => {
-            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
-
-        })
-    })
-
-    return songs
-}
-
-const playMusic = (track, pause = false) => {
-    currentSong.src = `/${currFolder}/` + track
-    if (!pause) {
-        currentSong.play()
-        play.src = "img/pause.svg"
-    }
-    document.querySelector(".songinfo").innerHTML = decodeURI(track)
-    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
-
-
-}
-
-async function displayAlbums() {
-    console.log("displaying albums")
-    let a = await fetch(`/songs/`)
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let anchors = div.getElementsByTagName("a")
-    let cardContainer = document.querySelector(".cardContainer")
-    let array = Array.from(anchors)
-    for (let index = 0; index < array.length; index++) {
-        const e = array[index]; 
-        if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
-            let folder = e.href.split("/").slice(-2)[0]
-            let a = await fetch(`/songs/${folder}/info.json`)
-            let response = await a.json(); 
-            cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
-            <div class="play">
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M5 20V4L19 12L5 20Z" stroke="#141B34" fill="#000" stroke-width="1.5"
-                        stroke-linejoin="round" />
-                </svg>
-            </div>
-
-            <img src="/songs/${folder}/cover.jpg" alt="">
-            <h2>${response.title}</h2>
-            <p>${response.description}</p>
-        </div>`
-        }
-    }
-    Array.from(document.getElementsByClassName("card")).forEach(e => { 
-        e.addEventListener("click", async item => {
-            console.log("Fetching Songs")
-            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`)  
-            playMusic(songs[0])
-
-        })
-    })
-}
-
-async function main() {
-    await getSongs("songs/ncs")
-    playMusic(songs[0], true)
-    await displayAlbums()
-
-    play.addEventListener("click", () => {
-        if (currentSong.paused) {
-            currentSong.play()
-            play.src = "img/pause.svg"
-        }
-        else {
-            currentSong.pause()
-            play.src = "img/play.svg"
-        }
-    })
-    currentSong.addEventListener("timeupdate", () => {
-        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
-        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
-    })
-    document.querySelector(".seekbar").addEventListener("click", e => {
-        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
-        document.querySelector(".circle").style.left = percent + "%";
-        currentSong.currentTime = ((currentSong.duration) * percent) / 100
-    })
-    document.querySelector(".hamburger").addEventListener("click", () => {
-        document.querySelector(".left").style.left = "0"
-    })
-    document.querySelector(".close").addEventListener("click", () => {
-        document.querySelector(".left").style.left = "-120%"
-    })
-    previous.addEventListener("click", () => {
-        currentSong.pause()
-        console.log("Previous clicked")
-        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
-        if ((index - 1) >= 0) {
-            playMusic(songs[index - 1])
-        }
-    })
-    next.addEventListener("click", () => {
-        currentSong.pause()
-        console.log("Next clicked")
-
-        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
-        if ((index + 1) < songs.length) {
-            playMusic(songs[index + 1])
-        }
-    })
-    document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e) => {
-        console.log("Setting volume to", e.target.value, "/ 100")
-        currentSong.volume = parseInt(e.target.value) / 100
-        if (currentSong.volume >0){
-            document.querySelector(".volume>img").src = document.querySelector(".volume>img").src.replace("mute.svg", "volume.svg")
-        }
-    })
-    document.querySelector(".volume>img").addEventListener("click", e=>{ 
-        if(e.target.src.includes("volume.svg")){
-            e.target.src = e.target.src.replace("volume.svg", "mute.svg")
-            currentSong.volume = 0;
-            document.querySelector(".range").getElementsByTagName("input")[0].value = 0;
-        }
-        else{
-            e.target.src = e.target.src.replace("mute.svg", "volume.svg")
-            currentSong.volume = .10;
-            document.querySelector(".range").getElementsByTagName("input")[0].value = 10;
-        }
-
-    })
-
-
-
-
-
-}
-
-main() 
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+  
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true,
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+  
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      },
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+      pinType: document.querySelector("#main").style.transform
+        ? "transform"
+        : "fixed",
+    });
+  
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  
+    ScrollTrigger.refresh();
+  }
+  function loadingAnimation() {
+    var tl = gsap.timeline();
+    tl.from(".line h1", {
+      y: 150,
+      stagger: 0.25,
+      duration: 0.6,
+      delay: 0.5,
+    });
+    tl.from("#line1-part1", {
+      opacity: 0,
+      onStart: function () {
+        var h5timer = document.querySelector("#line1-part1 h5");
+        var grow = 0;
+        setInterval(function () {
+          if (grow < 100) {
+            h5timer.innerHTML = grow++;
+          } else {
+            h5timer.innerHTML = grow;
+          }
+        }, 27);
+      },
+    });
+    tl.to(".line h2", {
+      animationName: "loaderAnime",
+      opacity: 1,
+    });
+    tl.to("#loader", {
+      opacity: 0,
+      duration: 0.2,
+      delay: 2.6,
+    });
+    tl.from("#page1", {
+      delay: 0.1,
+      y: 1600,
+      duration: 0.5,
+      ease: Power4,
+    });
+    tl.to("#loader", {
+      display: "none",
+    });
+    tl.from("#nav", {
+      opacity: 0,
+    });
+    tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
+      y: 140,
+      stagger: 0.2,
+    });
+    tl.from(
+      "#hero1, #page2",
+      {
+        opacity: 0,
+      },
+      "-=1.2"
+    );
+  }
+  function cursorAnimation() {
+    Shery.mouseFollower({
+      skew: true,
+      ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+      duration: 1,
+    });
+    Shery.makeMagnet("#nav-part2 h4");
+  
+    var videoContainer = document.querySelector("#video-container");
+    var video = document.querySelector("#video-container video");
+    videoContainer.addEventListener("mouseenter", function () {
+      videoContainer.addEventListener("mousemove", function (dets) {
+        gsap.to(".mousefollower", {
+          opacity: 0,
+        });
+        gsap.to("#video-cursor", {
+          left: dets.x - 570,
+          y: dets.y - 300,
+        });
+      });
+    });
+    videoContainer.addEventListener("mouseleave", function () {
+      gsap.to(".mousefollower", {
+        opacity: 1,
+      });
+      gsap.to("#video-cursor", {
+        left: "70%",
+        top: "-15%",
+      });
+    });
+  
+    var flag = 0;
+    videoContainer.addEventListener("click", function () {
+      if (flag == 0) {
+        video.play();
+        video.style.opacity = 1;
+        document.querySelector(
+          "#video-cursor"
+        ).innerHTML = `<i class="ri-pause-mini-fill"></i>`;
+        gsap.to("#video-cursor", {
+          scale: 0.5,
+        });
+        flag = 1;
+      } else {
+        video.pause();
+        video.style.opacity = 0;
+        document.querySelector(
+          "#video-cursor"
+        ).innerHTML = `<i class="ri-play-mini-fill"></i>`;
+        gsap.to("#video-cursor", {
+          scale: 1,
+        });
+        flag = 0;
+      }
+    });
+  }
+  function sheryAnimation() {
+    Shery.imageEffect(".image-div", {
+      style: 5,
+      gooey: true,
+      // debug:true,
+      config: {
+        a: { value: 2, range: [0, 30] },
+        b: { value: 0.75, range: [-1, 1] },
+        zindex: { value: -9996999, range: [-9999999, 9999999] },
+        aspect: { value: 0.7241195453907675 },
+        gooey: { value: true },
+        infiniteGooey: { value: false },
+        growSize: { value: 4, range: [1, 15] },
+        durationOut: { value: 1, range: [0.1, 5] },
+        durationIn: { value: 1.5, range: [0.1, 5] },
+        displaceAmount: { value: 0.5 },
+        masker: { value: true },
+        maskVal: { value: 1.23, range: [1, 5] },
+        scrollType: { value: 0 },
+        geoVertex: { range: [1, 64], value: 1 },
+        noEffectGooey: { value: true },
+        onMouse: { value: 0 },
+        noise_speed: { value: 0.5, range: [0, 10] },
+        metaball: { value: 0.33, range: [0, 2] },
+        discard_threshold: { value: 0.5, range: [0, 1] },
+        antialias_threshold: { value: 0.01, range: [0, 0.1] },
+        noise_height: { value: 0.5, range: [0, 2] },
+        noise_scale: { value: 10, range: [0, 100] },
+      },
+    });
+  }
+  function flagAnimation() {
+    document.addEventListener("mousemove", function (dets) {
+      gsap.to("#flag", {
+        x: dets.x,
+        y: dets.y,
+      });
+    });
+    document.querySelector("#hero3").addEventListener("mouseenter", function () {
+      gsap.to("#flag", {
+        opacity: 1,
+      });
+    });
+    document.querySelector("#hero3").addEventListener("mouseleave", function () {
+      gsap.to("#flag", {
+        opacity: 0,
+      });
+    });
+  }
+  function footerAnimation() {
+    var clutter = "";
+    var clutter2 = "";
+    document
+      .querySelector("#footer h1")
+      .textContent.split("")
+      .forEach(function (elem) {
+        clutter += `<span>${elem}</span>`;
+      });
+    document.querySelector("#footer h1").innerHTML = clutter;
+    document
+      .querySelector("#footer h2")
+      .textContent.split("")
+      .forEach(function (elem) {
+        clutter2 += `<span>${elem}</span>`;
+      });
+    document.querySelector("#footer h2").innerHTML = clutter2;
+  
+    document
+      .querySelector("#footer-text")
+      .addEventListener("mouseenter", function () {
+        gsap.to("#footer h1 span", {
+          opacity: 0,
+          stagger: 0.05,
+        });
+        gsap.to("#footer h2 span", {
+          delay: 0.35,
+          opacity: 1,
+          stagger: 0.1,
+        });
+      });
+    document
+      .querySelector("#footer-text")
+      .addEventListener("mouseleave", function () {
+        gsap.to("#footer h1 span", {
+          opacity: 1,
+          stagger: 0.1,
+          delay: 0.35,
+        });
+        gsap.to("#footer h2 span", {
+          opacity: 0,
+          stagger: 0.05,
+        });
+      });
+  }
+  
+  loadingAnimation();
+  cursorAnimation();
+  locomotiveAnimation();
+  sheryAnimation();
+  flagAnimation();
+  footerAnimation();
